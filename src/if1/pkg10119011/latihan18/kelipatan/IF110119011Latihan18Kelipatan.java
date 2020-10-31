11d014dd251e3c951947f489fd0064598ec61106
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-31 14:35:28 +0700

    IF1-10119011-Latihan18-Kelipatan

---
+++ src/if1/pkg10119011/latihan18/kelipatan/IF110119011Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for( double i = 3.5; i<=35; i+=3.5) {
+            System.out.println(i);
+        }
+    }
+    
+}
